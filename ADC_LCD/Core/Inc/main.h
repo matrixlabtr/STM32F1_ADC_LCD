e3e7651066e5f96539ec9020003f5f1900539cfe
Sadıkhan <sadikhandogdu@gmail.com>
2022-11-27 18:38:04 +0300

    LCD Scanner implementation

--- ADC_LCD/Core/Inc/main.h
+++ ADC_LCD/Core/Inc/main.h
@@ -59,18 +59,6 @@ void Error_Handler(void);
 /* Private defines -----------------------------------------------------------*/
 #define LED_Pin GPIO_PIN_13
 #define LED_GPIO_Port GPIOC
-#define LCD_RS_Pin GPIO_PIN_1
-#define LCD_RS_GPIO_Port GPIOA
-#define LCD_EN_Pin GPIO_PIN_2
-#define LCD_EN_GPIO_Port GPIOA
-#define LCD_D4_Pin GPIO_PIN_4
-#define LCD_D4_GPIO_Port GPIOA
-#define LCD_D5_Pin GPIO_PIN_5
-#define LCD_D5_GPIO_Port GPIOA
-#define LCD_D6_Pin GPIO_PIN_6
-#define LCD_D6_GPIO_Port GPIOA
-#define LCD_D7_Pin GPIO_PIN_7
-#define LCD_D7_GPIO_Port GPIOA
 /* USER CODE BEGIN Private defines */
 
 /* USER CODE END Private defines */
